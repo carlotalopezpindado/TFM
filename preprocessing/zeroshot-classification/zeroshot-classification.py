47d6d9b7678b3560186c363a6fc82997f57a2b92
Carlota López <carlota.lopezpindado@gmail.com>
2024-05-05 11:59:11 +0200

    indices en elastic search

--- preprocessing/zeroshot-classification/zeroshot-classification.py
+++ preprocessing/zeroshot-classification/zeroshot-classification.py
@@ -1,46 +1,42 @@
 import pandas as pd
-from transformers import AutoModelForSequenceClassification, AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoModelForSequenceClassification, AutoTokenizer
 import torch
 
+## ---------------- DATOS CONFIGURACIÓN INICIAL -------------------------
 import configparser
 config = configparser.ConfigParser()
-config.read('config.ini')
 
+model_name_classify = config['classification']['model_name_classify']
+
+## -----------------------------------------------------------------------
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model_name_translate = config['classification']['Helsinki-NLP/opus-mt-es-en']
-tokenizer_translate = AutoTokenizer.from_pretrained(model_name_translate)
-model_translate = AutoModelForSeq2SeqLM.from_pretrained(model_name_translate).to(device)
-
-model_name_classify = config['classification']['facebook/bart-large-mnli']
 tokenizer_classify = AutoTokenizer.from_pretrained(model_name_classify)
 model_classify = AutoModelForSequenceClassification.from_pretrained(model_name_classify).to(device)
 
-def translate_text(text):
-    translated = tokenizer_translate(text, return_tensors='pt', padding=True, truncation=True, max_length=512).to(device)
-    translated_text = model_translate.generate(**translated)
-    return tokenizer_translate.decode(translated_text[0], skip_special_tokens=True)
-
 def classify_text(text, labels):
-    inputs = tokenizer_classify(text, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device)
-    with torch.no_grad():
+    inputs = tokenizer_classify(text, return_tensors="pt", padding=True, truncation=True, max_length=512).to(device) # Tokenizamos el texto
+    with torch.no_grad(): # No calculamos gradientes ya que no vamos a entrenar
         logits = model_classify(**inputs).logits
-    scores = torch.softmax(logits, dim=1)
-    results = sorted([(label, score.item()) for label, score in zip(labels, scores[0])], key=lambda x: x[1], reverse=True)  
-    return results[0][0]  
 
-clean_data_path = config['classification']['clean_data_path']
+    scores = torch.softmax(logits, dim=1) # Aplicamos softmax para obtener las probabilidades
+    
+    results = sorted([(label, score.item()) for label, score in zip(labels, scores[0])], key=lambda x: x[1], reverse=True)
+    
+    return results[0][0]
+
+clean_data_path = 'preprocessing/scrapping/clean_data.parquet'
 df = pd.read_parquet(clean_data_path)
 
-labels = config['classification']['labels']
+labels = ["Legislación y Regulación", "Administración Pública y Procedimientos", "Educación y Cultura"]
 
 classification_results = []
 
 for index, row in df.iterrows():
-    text_es = row['text'] 
-    text_en = translate_text(text_es)
-    classification_label = classify_text(text_en, labels)
+    print(index)
+    text = row['text'] 
+    classification_label = classify_text(text, labels)  
     classification_results.append(classification_label)
-
+    
 df['classification_result'] = classification_results
 df.to_parquet('classified_data.parquet')
--- processing/indexing.py
+++ processing/indexing.py
@@ -2,52 +2,43 @@ import pandas as pd
 from transformers import BitsAndBytesConfig
 from llama_index.core import PromptTemplate, Settings, VectorStoreIndex, Document, StorageContext
 from llama_index.llms.huggingface import HuggingFaceLLM
-from llama_index.vector_stores.postgres import PGVectorStore
 import torch
-import psycopg2
-from sqlalchemy import make_url
 import configparser
+from llama_index.core import VectorStoreIndex
+from llama_index.vector_stores.elasticsearch import ElasticsearchStore
 
 ## ---------------- DATOS CONFIGURACIÓN INICIAL -------------------------
 config = configparser.ConfigParser()
 config.read('config.ini')
+
+model_name = config['indexing']['indexing_model']
+context_window = int(config['indexing']['context_window'])
+max_new_tokens = int(config['indexing']['max_new_tokens'])
 temp = config['indexing']['temperature']
 top_k = config['indexing']['top_k']
 top_p = config['indexing']['top_p']
-connection_string = config['indexing']['postgres']
-db_name = config['indexing']['db_name']
-model_name = config['indexing']['indexing_model']
-context_window = config['indexing']['context_window']
-max_new_tokens = config['indexing']['max_new_tokens']
 embed_model = config['indexing']['embed_model']
-classified_data_path = config['indexing']['classified_data_path'] 
 
 ## ---------------- CONSTANTES ------------------------------------------
 quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dtype=torch.float16, bnb_4bit_quant_type="nf4", bnb_4bit_use_double_quant=True)
 
-labels = ["Legislation and Regulation", "Public Administration and Procedures", "Education and Culture"] 
-
-labels_dic = {
-    "Legislation and Regulation": "Regulación y Legislación",
-    "Public Administration and Procedures": "Administración Pública y Procedimientos",
-    "Education and Culture": "Educación y Cultura"
-}
+labels = ["Regulación y Legislación", "Administración Pública y Procedimientos", "Educación y Cultura"] 
 
 system_prompt = """
-    Estas asistiendo a una persona española que no habla inglés, por lo que solo puedes responder en español. 
+    Las personas a las que asistes únicamente hablan el idioma en el que preguntan, por lo que siempre tienes que responder en el mismo idioma que se te haga la pregunta. 
+    Esto es muy importante. Si no, no entenderán tus respuestas.
     Estás asistiendo en consultas sobre documentos del Boletín Oficial del Estado (BOE), clasificados en una de las siguientes categorías: 
-    Legislación y Regulación, Administración Pública y Procedimientos o Educación y Cultura. Siempre responde en castellano con información 
+    Legislación y Regulación, Administración Pública y Procedimientos o Educación y Cultura. Siempre responde con información 
     relevante y precisa. Utiliza únicamente la información contenida en los documentos disponibles. Si la información no está disponible o 
     la pregunta excede el alcance de tu conocimiento actual, informa al usuario de manera clara y directa, evitando especulaciones o suposiciones.
     Responde con la información más actualizada que tengas acerca de la pregunta."""
 
 ## ---------------------------------------------------------------------
-def initialize_llm(english_label, system_prompt):
-    spanish_label = labels_dic[english_label]  
+def initialize_llm(label, system_prompt):
     query_wrapper = PromptTemplate(
-        f"<s> [INST] [Consulta de {spanish_label}] {system_prompt} Dada la siguiente pregunta del usuario relacionada con la categoría {spanish_label} "
+        f"<s> [INST] [Consulta de {label}] {system_prompt} Dada la siguiente pregunta del usuario relacionada con la categoría {label} "
         "de documentos del BOE, proporciona una respuesta detallada y específica utilizando la información contenida en los documentos. "
-        "Asegúrate de incluir referencias al documento o documentos relevantes cuando sea posible. Responde siempre en español.\nPregunta: {query_str} [/INST] </s>\n"
+        "Asegúrate de incluir referencias al documento o documentos relevantes cuando sea posible.\nPregunta: {query_str} [/INST] </s>\n"
     )
     
     llm = HuggingFaceLLM(                                                        
@@ -63,23 +54,21 @@ def initialize_llm(english_label, system_prompt):
     )
     return llm
 
-def init_save_index(document_objects, llm, english_label):
-    spanish_label = labels_dic[english_label]
+def init_save_index(document_objects, llm, label):
     Settings.llm = llm
     Settings.embed_model = embed_model 
-    
-    url = make_url(connection_string)
-    vector_store = PGVectorStore.from_params(database=db_name, host=url.host, password=url.password, port=url.port, user=url.username, table_name=spanish_label.replace(" ", "_").lower(), embed_dim=768, hybrid_search=True, text_search_config="spanish")
+
+    vector_store = ElasticsearchStore(
+        es_url="https://localhost:9200",
+        index_name=label.replace(" ", "_").lower()
+    )
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
     print("Indexando y guardando")
     VectorStoreIndex.from_documents(document_objects, storage_context=storage_context, show_progress=True)
                 
 def main():       
-    conn = psycopg2.connect(connection_string)
-    conn.autocommit = True 
-            
-    df = pd.read_parquet(classified_data_path)
+    df = pd.read_parquet('preprocessing/zeroshot-classification/classified_data.parquet')
     grouped = df.groupby('classification_result')
 
     for classification, group in grouped:
@@ -88,7 +77,7 @@ def main():
         
         llm = initialize_llm(classification, system_prompt)
         
-        document_objects = [Document(doc_id=doc['url'], text=doc['text']) for doc in documents]
+        document_objects = [Document(text=doc['text'], metadata = {"filename": doc['url']}) for doc in documents]
         init_save_index(document_objects, llm, classification)
         
 if __name__ == "__main__":
