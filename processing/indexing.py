280e6eb5ec099f8a7f34c172ff95b8c4b691c741
Carlota López <carlota.lopezpindado@gmail.com>
2024-05-08 19:31:41 +0200

    indices en faiss

--- processing/indexing.py
+++ processing/indexing.py
@@ -1,12 +1,12 @@
+import os
 import pandas as pd
 from transformers import BitsAndBytesConfig
 from llama_index.core import PromptTemplate, Settings, VectorStoreIndex, Document, StorageContext
 from llama_index.llms.huggingface import HuggingFaceLLM
 import torch
 import configparser
-from llama_index.core import VectorStoreIndex
-from llama_index.vector_stores.elasticsearch import ElasticsearchStore
-from elasticsearch import Elasticsearch
+import faiss
+from llama_index.vector_stores.faiss import FaissVectorStore
 
 ## ---------------- DATOS CONFIGURACIÓN INICIAL -------------------------
 config = configparser.ConfigParser()
@@ -26,22 +26,24 @@ quantization_config = BitsAndBytesConfig(load_in_4bit=True, bnb_4bit_compute_dty
 labels = ["Regulación y Legislación", "Administración Pública y Procedimientos", "Educación y Cultura"] 
 
 system_prompt = """
-    Las personas a las que asistes únicamente hablan el idioma en el que preguntan, por lo que siempre tienes que responder en el mismo idioma que se te haga la pregunta. 
-    Esto es muy importante. Si no, no entenderán tus respuestas.
-    Estás asistiendo en consultas sobre documentos del Boletín Oficial del Estado (BOE), clasificados en una de las siguientes categorías: 
-    Legislación y Regulación, Administración Pública y Procedimientos o Educación y Cultura. Siempre responde con información 
-    relevante y precisa. Utiliza únicamente la información contenida en los documentos disponibles. Si la información no está disponible o 
-    la pregunta excede el alcance de tu conocimiento actual, informa al usuario de manera clara y directa, evitando especulaciones o suposiciones.
-    Responde con la información más actualizada que tengas acerca de la pregunta."""
+    Las personas a las que asistes únicamente hablan el idioma en el que preguntan, por lo que siempre tienes que responder 
+    en el mismo idioma que se te haga la pregunta. Esto es muy importante. Si no, no entenderán tus respuestas.
+    Estás asistiendo en consultas sobre documentos del Boletín Oficial del Estado (BOE). Responde con información relevante 
+    y precisa, utilizando únicamente la información contenida en los documentos disponibles. Si la información no está 
+    disponible o la pregunta excede el alcance de tu conocimiento actual, informa al usuario de manera clara y directa, 
+    evitando especulaciones o suposiciones. Proporciona una respuesta detallada y específica a la pregunta del usuario, 
+    incluyendo referencias al documento o documentos relevantes cuando sea posible.
+    Responde con la información más actualizada que tengas acerca de la pregunta.
+"""
 
+embeddings_dimension = 768
+faiss_index = faiss.IndexFlatL2(embeddings_dimension)
 ## ---------------------------------------------------------------------
-def initialize_llm(label, system_prompt):
+def init_llm():
     query_wrapper = PromptTemplate(
-        f"<s> [INST] [Consulta de {label}] {system_prompt} Dada la siguiente pregunta del usuario relacionada con la categoría {label} "
-        "de documentos del BOE, proporciona una respuesta detallada y específica utilizando la información contenida en los documentos. "
-        "Asegúrate de incluir referencias al documento o documentos relevantes cuando sea posible.\nPregunta: {query_str} [/INST] </s>\n"
+    f"<s> [INST] {system_prompt} Dada la siguiente pregunta del usuario, proporciona una respuesta detallada y específica utilizando la información contenida en los documentos. "
+    "Asegúrate de incluir referencias al documento o documentos relevantes cuando sea posible.\nPregunta: {{query_str}} [/INST] </s>\n"
     )
-    
     llm = HuggingFaceLLM(                                                        
         model_name = model_name,                                                 
         tokenizer_name = model_name,                                             
@@ -59,34 +61,31 @@ def init_save_index(document_objects, llm, label):
     Settings.llm = llm
     Settings.embed_model = embed_model 
 
-    es_client = Elasticsearch(
-        hosts=["https://localhost:9200"],
-        basic_auth=("elastic", "qTIql*fRyWF*q=6OJBal"),  # Reemplaza con tus credenciales
-        verify_certs=False,
-        ssl_show_warn=False
-    )
-
-    # Use the Elasticsearch client in the vector store
-    vector_store = ElasticsearchStore(
-        es_client=es_client,
-        index_name=label.replace(" ", "_").lower()
-    )
+    vector_store = FaissVectorStore(faiss_index=faiss_index)
     storage_context = StorageContext.from_defaults(vector_store=vector_store)
 
     print("Indexando y guardando")
-    VectorStoreIndex.from_documents(document_objects, storage_context=storage_context, show_progress=True)
+    
+    safe_label = label.replace(" ", "_").lower()
+    safe_label = safe_label.replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u").replace("ñ", "n")
+    directory = f'processing/indexes/{safe_label}'
+    os.makedirs(directory, exist_ok=True)
+    
+    index = VectorStoreIndex.from_documents(document_objects, storage_context=storage_context, show_progress=True)
+    index.storage_context.persist(directory)
                 
 def main():       
     df = pd.read_parquet('classified_data.parquet')
     grouped = df.groupby('classification_result')
 
+    llm = init_llm()
+    
     for classification, group in grouped:
         print(classification)
+        
         documents = group[['url', 'text']].to_dict(orient='records')
-        
-        llm = initialize_llm(classification, system_prompt)
-        
-        document_objects = [Document(text=doc['text'], metadata = {"filename": doc['url']}) for doc in documents]
+
+        document_objects = [Document(text=doc['text'], metadata={"filename": doc['url']}) for doc in documents]
         init_save_index(document_objects, llm, classification)
         
 if __name__ == "__main__":
